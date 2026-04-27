65d5d5a582288035c49ed20a6b586a8805fa06d4
lamctsopro123-spec <lamctsopro123@gmail.com>
2026-04-27 00:21:17 -0800

    Update Library_Original.lua

--- Library_Original.lua
+++ Library_Original.lua
@@ -73,20 +73,12 @@ task.spawn(function()
 end)
 
 task.spawn(function()
-    local CoreGui = game:GetService("CoreGui")
-    local overlay = CoreGui:WaitForChild("RobloxPromptGui"):WaitForChild("promptOverlay")
 
-    overlay.ChildAdded:Connect(function(child)
-        if child.Name == 'ErrorPrompt' then
-            while true do
-                TeleportService:Teleport(3260590327)
-                task.wait(5)
-            end
-        end
-    end)
-end)
 
-task.spawn(function()
+
+
+
+
     pcall(function()
         RemoteFunc:InvokeServer("Settings", "Update", "Show Nametags", false)
     end)
@@ -149,6 +141,7 @@ local SellFarmsRunning = false
 local AutoGatlingRunning = false
 local GatlingExecuted = false
 local AutoPremiumRunning = false
+local StackerErrorShown = false
 
 local MaxPathDistance = 300 -- default
 local MilMarker = nil
@@ -3064,7 +3057,7 @@ function TDS:Loadout(...)
     end
 
     local towers = {...}
-    local remote = game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunction")
+    local remote = game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvent")
     local StateReplicators = ReplicatedStorage:FindFirstChild("StateReplicators")
 
     local CurrentlyEquipped = {}
@@ -3178,7 +3171,7 @@ function TDS:GameInfo(name, list)
 
     CastModifierVote(modifiers)
 
-    if MarketplaceService:UserOwnsGamePassAsync(LocalPlayer.UserId, 10518590) then
+    if MarketplaceService:UserOwnsGamePassAsync(LocalPlayer.UserId, 10518590) or game:GetService("ReplicatedStorage").StateReplicators.GameStateReplicator:GetAttribute("IsPrivateServer") == true then
         SelectMapOverride(name, "vip")
         Logger:Log("Selected map: " .. name)
         repeat task.wait(1) until PlayerGui:FindFirstChild("ReactUniversalHotbar")
